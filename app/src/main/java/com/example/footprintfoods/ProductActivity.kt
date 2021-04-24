d2a5acca9d3e2dcdf738df1d6d55772e63a1213b
Vinay <vinay.shah@hotmail.co.uk>
2021-04-24 20:13:57 +0100

    Radio Buttons from DB

--- app/src/main/java/com/example/footprintfoods/ProductActivity.kt
+++ app/src/main/java/com/example/footprintfoods/ProductActivity.kt
@@ -1,15 +1,14 @@
 package com.example.footprintfoods
 
+import android.content.ContentValues
 import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import android.widget.RadioGroup
-import android.widget.TextView
-import android.widget.Toolbar
+import android.view.ViewGroup
+import android.widget.*
 import com.bumptech.glide.Glide
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.appbar.MaterialToolbar
@@ -21,6 +20,7 @@ class ProductActivity : AppCompatActivity() {
     private val db = Firebase.firestore
     private lateinit var prodDescription: String
     private lateinit var prodStorage: String
+    var produceFarmers = arrayListOf<String>()
     // Setup Market DB variables
     private lateinit var productTitle: String
     private lateinit var productCarbon: String
@@ -29,6 +29,8 @@ class ProductActivity : AppCompatActivity() {
     private lateinit var marketTitle: String
     private lateinit var productCategory: String
     private lateinit var transitionImage: String
+    // Setup RadioGroup
+    lateinit var farmerRadioGroup: RadioGroup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set theme & views
@@ -48,13 +50,16 @@ class ProductActivity : AppCompatActivity() {
         }
         // Pull from DB
         databasePull()
+        databasePulFarmer()
         // Setup Variables
         val prodImage = findViewById<ImageView>(R.id.productDetailsImage)
         val prodTitle = findViewById<MaterialToolbar>(R.id.productDetailsTitle)
         val prodDesc = findViewById<TextView>(R.id.productDetailsDescription)
-        val prodFarmers = findViewById<RadioGroup>(R.id.productDetailsFarmerRadio)
         val prodStorage = findViewById<TextView>(R.id.productDetailsUsageDescription)
         val toolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.productDetailsToolbarLayout)
+        val minusButton = findViewById<Button>(R.id.productDetailsMinus)
+        val plusButton = findViewById<Button>(R.id.productDetailsPlus)
+        val prodQuantity = findViewById<TextView>(R.id.productDetailsQuantityText)
         // Initialise variables
         Glide.with(this).load(productUrl).into(prodImage)
         prodTitle.title = productTitle
@@ -66,7 +71,23 @@ class ProductActivity : AppCompatActivity() {
         prodTitle.setNavigationOnClickListener {
             supportFinishAfterTransition()
         }
+        minusButton.setOnClickListener{
+            var quantity: Int = prodQuantity.text.toString().toInt()
+            if (quantity > 1){
+                quantity -= 1
+            }
+            prodQuantity.text = quantity.toString()
+        }
+        plusButton.setOnClickListener{
+            var quantity: Int = prodQuantity.text.toString().toInt()
+            quantity += 1
+            prodQuantity.text = quantity.toString()
+        }
         if (marketTitle == "Loughborough Farmers' and Craft Market") {
+            toolbarLayout.setExpandedTitleColor(getColor(R.color.white))
+            toolbarLayout.setCollapsedTitleTextColor(getColor(R.color.white))
+            prodTitle.navigationIcon?.setTint(getColor(R.color.white))
+        } else if (marketTitle == "Notting Hill Farmers' Market"){
             toolbarLayout.setExpandedTitleColor(getColor(R.color.white))
             toolbarLayout.setCollapsedTitleTextColor(getColor(R.color.white))
             prodTitle.navigationIcon?.setTint(getColor(R.color.white))
@@ -75,9 +96,36 @@ class ProductActivity : AppCompatActivity() {
             prodTitle.navigationIcon?.setTint(getColor(R.color.black))
         }
         Handler().postDelayed({
+            createFarmerRadio()
             prodDesc.text = prodDescription
             prodStorage.text = this.prodStorage
             supportStartPostponedEnterTransition()}, 200)
+    }
+    // Create radio buttons
+    private fun createFarmerRadio() {
+        var i = 0
+        farmerRadioGroup = findViewById(R.id.productDetailsFarmerRadio)
+        for (farmer in produceFarmers){
+            val name = "\u0009" + farmer.substring(0, farmer.indexOf("/"))
+            val rem = farmer.substring(farmer.indexOf("/") + 1, farmer.length)
+            val price = "£" + rem.substring(0, rem.indexOf("/")) + "/kg"
+            val carbon = rem.substring(rem.indexOf("/") + 1, rem.length) + " CO₂e"
+            val concat = String.format("%s \u0009 \u0009 \u0009 " +
+                    "\u0009 \u0009 \u0009 " +
+                    "\u0009 \u0009 \u0009 " +
+                    "\u0009 \u0009 \u0009 %s", name, price)
+            val concat2 = String.format("%s \u0009 \u0009 \u0009 %s", concat, carbon)
+            Log.d("Product Activity", concat)
+            val radioButton = RadioButton(this)
+            radioButton.layoutParams = LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+            radioButton.text = concat2
+            //radioButton.setTextAppearance(R.style.TextAppearance_FootprintFoods_Body1)
+            radioButton.id = i
+            farmerRadioGroup.addView(radioButton)
+            i += 1
+        }
     }
     // Database pull function
     private fun databasePull() {
@@ -91,5 +139,31 @@ class ProductActivity : AppCompatActivity() {
                     prodDescription = result?.get("Description").toString()
                     prodStorage = result?.get("Storage").toString()
                 }
+        Log.d("Product Activity", "Done from DB")
+    }
+    // Database farmer pull function
+    private fun databasePulFarmer() {
+        Log.d("Product Activity", "Starting pulling Produce from DB")
+        db.collection("Foodmarkets")
+                .document(marketTitle)
+                .collection(productCategory)
+                .document(productTitle)
+                .collection("Seller")
+                .get()
+                .addOnSuccessListener { result ->
+                    if (result != null) {
+                        for (document in result) {
+                            val farmer = document.id
+                            val farmerCarbon = document.get("Carbon").toString()
+                            val farmerPrice = document.get("Price").toString()
+                            val concat = "$farmer/${farmerPrice}/${farmerCarbon}"
+                            Log.d("Product Activity", concat)
+                            produceFarmers.add(concat)
+                        }
+                    } else {
+                        Log.d("Product Activity", "No results found")
+                    }
+                }
+        Log.d("Product Activity", "Done from DB")
     }
 }
